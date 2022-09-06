f1e33d7551784afb67523b6b8a19794c2fa55e55
Jacob Allen <jallen1887@gmail.com>
2022-09-06 00:43:38 -0400

    Add slider control for grid boxes

--- scripts.js
+++ scripts.js
@@ -1,17 +1,23 @@
 function createGrid(gridBoxNumber) {
     const gridContainer = document.querySelector('#grid-container');
+    const gridBoxSize = findGridBoxSize(gridBoxNumber);
     
     for (let i = 0; i < gridBoxNumber**2; i++) {
         const gridBox = document.createElement('div');
         gridBox.classList.add('grid-box');
 
-        gridBox.style.width = findGridBoxSize(gridBoxNumber);
-        gridBox.style.height = findGridBoxSize(gridBoxNumber);
+        gridBox.style.width = gridBoxSize;
+        gridBox.style.height = gridBoxSize;
         gridContainer.appendChild(gridBox);
-        gridBox.addEventListener('mousedown', () => gridBox.style.backgroundColor = 'blue');
+        gridBox.addEventListener('mouseover', changeColor);
+        
     }
 
 
+}
+function changeColor(e) {
+        e.target.style.backgroundColor = 'blue';
+    
 }
 
 function findGridBoxSize(gridBoxNumber){
@@ -20,4 +26,26 @@ function findGridBoxSize(gridBoxNumber){
     return sizeText;
 }
 
-createGrid(10);
+function removeGrid() {
+    const gridContainer = document.querySelector('#grid-container');
+    let gridBoxes = document.querySelectorAll('.grid-box');
+
+    gridBoxes.forEach((gridBox) => {
+        gridContainer.removeChild(gridBox);
+    })
+}
+
+const slider = document.querySelector('#grid-slider');
+let output = document.querySelector('#grid-size-text');
+output.textContent = `${slider.value} x ${slider.value}`;
+createGrid(slider.value);
+slider.oninput = function() {
+    output.textContent = `${this.value} x ${this.value}`;
+}
+slider.onchange = function() {
+    removeGrid();
+    createGrid(this.value);
+    
+}
+
+
